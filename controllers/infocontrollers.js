5aae648a02e2fb786c1feb24324e242d70bc7dd7
shaban00 <shaban00hassan@gmail.com>
2018-10-24 06:36:51 -0400

    Files modified

--- controllers/infocontrollers.js
+++ controllers/infocontrollers.js
@@ -4,6 +4,12 @@ var lodash = require('lodash');
 var countries = require('../models/countries.json');
 
 
+var notFound = function(res){
+	res.json(404, {
+		message: "Sorry, page does not exist",
+		code: 34
+	});
+}
 
 exports.getIndex = function(req, res){
 	res.send({message: 'Welcome to countryinfo where you get a lot of information about the countries you want'});
@@ -14,3 +20,15 @@ exports.getAllCountries =  function(req, res){
 	//res.status(200).json(countries);
 	res.json(countries);
 };
+
+exports.capital = function(req, res, capital){
+
+	var country = lodash.find(countries, function(mycapital){
+		return validator.isIn(capital, mycapital.capital)
+	});
+
+	if (!country) {
+		notFound(res);
+	}
+	res.status(200).json(country);
+}
--- routes/countryinfo.js
+++ routes/countryinfo.js
@@ -3,8 +3,17 @@ var countryinfo = express.Router();
 var countryinfocontrollers = require('../controllers/infocontrollers');
 
 
+
+countryinfo.get('/', function(req, res){
+	countryinfocontrollers.getIndex(req, res)
+});
+
 countryinfo.get('/countryinfo/', function(req, res){
 	countryinfocontrollers.getAllCountries(req, res);
+});
+
+countryinfo.get('/countryinfo/capital/:capital', function(req, res){
+	countryinfocontrollers.capital(req, res, req.params.capital);
 });
 
 
